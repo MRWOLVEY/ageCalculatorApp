eb101ce500e8c8f1944966061e80b68c1b7586e8
Wolvey <wolfeyplays123@gmail.com>
2023-07-28 03:59:24 +0300

    not yet

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const currentDate=new Date();
+var currentDate=new Date();
 const currentYear=currentDate.getFullYear();
 const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const inputDays=document.querySelector("#input-days");
 const inputMonths=document.querySelector("#input-months");
 const inputYears=document.querySelector("#input-years");
+const button=document.querySelector("button");
 
 const outputDays=document.querySelector("#ouput-days");
 const outputMonths=document.querySelector("#ouput-months");
@@ -16,9 +17,38 @@ const init = () => {
     outputYears.innerHTML="- -";
 }
 
+
 var daysb4process=0;
 var monthsb4process=0;
 var yearsb4process=0;
+
+const calc=()=>{
+    currentDate= new Date();
+    daysb4process=inputDays.firstChild.value;
+    monthsb4process=inputMonths.firstChild.value;
+    yearsb4process=inputYears.firstChild.value;
+    pastDate= new Date();
+    pastDate.setFullYear(yearsb4process);
+    pastDate.setMonth(monthsb4process);
+    pastDate.setDate(daysb4process);
+    const timeDiff=currentDate-pastDate;
+    console.log([daysb4process,monthsb4process,yearsb4process]);
+    const msid = 1000 * 60 * 60 * 24;
+    const msim=msid*30.44;
+    const msiy=msid*365;
+    const years=Math.floor(timeDiff/msiy);
+    const months=Math.floor(timeDiff/msim);
+    const days=Math.floor(timeDiff/msid);
+    return [years,months,days];
+}
+
+const display = () => {
+    console.log(calc());
+    // const [years,months,days]=calc();
+    // outputDays.innerHTML=days;
+    // outputMonths.innerHTML=months;
+    // outputYears.innerHTML=years;
+}
 
 const emptyError = (e) => {
     if (e.target.value === "") {
@@ -90,3 +120,4 @@ inputYears.addEventListener("input",function(e){
     yearError(e);
 })
 
+button.addEventListener("click",display);
