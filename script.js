7203ffe6758d258ee15ad63e9fac8ea9384acfb8
Wolvey <wolfeyplays123@gmail.com>
2023-07-29 16:06:26 +0300

    added app logic, and it took foking forever

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var currentDate=new Date();
 const currentYear=currentDate.getFullYear();
 const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let okd=0;
+let okm=0;
+let oky=0;
 
 const inputDays=document.querySelector("#input-days");
 const inputMonths=document.querySelector("#input-months");
@@ -16,108 +19,114 @@ const init = () => {
     outputMonths.innerHTML="- -";
     outputYears.innerHTML="- -";
 }
+const calc = () =>{
+    if(okd+okm+oky==3){        
+        currentDate= new Date();
+        let day = currentDate.getDate();
+        let month = currentDate.getMonth()+1;
+        let year = currentDate.getFullYear();
 
+        let daysInp=inputDays.firstChild.value;  
+        let monthsInp=inputMonths.firstChild.value;  
+        let yearsInp=inputYears.firstChild.value; 
 
-var daysb4process=0;
-var monthsb4process=0;
-var yearsb4process=0;
+        if(daysInp > day){
+            day+=daysInMonth[month-1];
+            month-=1;
+        }
+        if(monthsInp > month){
+            month+=12;
+            year-=1;
+        }
 
-const calc=()=>{
-    currentDate= new Date();
-    daysb4process=inputDays.firstChild.value;
-    monthsb4process=inputMonths.firstChild.value;
-    yearsb4process=inputYears.firstChild.value;
-    pastDate= new Date();
-    pastDate.setFullYear(yearsb4process);
-    pastDate.setMonth(monthsb4process);
-    pastDate.setDate(daysb4process);
-    const timeDiff=currentDate-pastDate;
-    console.log([daysb4process,monthsb4process,yearsb4process]);
-    const msid = 1000 * 60 * 60 * 24;
-    const msim=msid*30.44;
-    const msiy=msid*365;
-    const years=Math.floor(timeDiff/msiy);
-    const months=Math.floor(timeDiff/msim);
-    const days=Math.floor(timeDiff/msid);
-    return [years,months,days];
-}
+        const d = day - daysInp;
+        const m = month - monthsInp;
+        const y = year - yearsInp;
 
+        return [y,m,d];
+    }
+        else{
+            init();
+            return [-1,-1,-1];
+        }
+}   
 const display = () => {
-    console.log(calc());
-    // const [years,months,days]=calc();
-    // outputDays.innerHTML=days;
-    // outputMonths.innerHTML=months;
-    // outputYears.innerHTML=years;
+    const [years,months,days]=calc();
+    if(years*months*days>=0){
+        outputDays.innerHTML=days;
+        outputMonths.innerHTML=months;
+        outputYears.innerHTML=years;
+    }
 }
 
 const emptyError = (e) => {
     if (e.target.value === "") {
-        // e.target.parentNode.classList.remove("error", "emptyError", "dayError", "monthError", "yearError", "dateError");
         e.target.parentNode.classList.add("error", "emptyError");
+        return 0;
     }
     else{
         e.target.parentNode.classList.remove("error", "emptyError");
+        return 1;
     }
 }
 const dayError = (e) => {
     if (e.target.value !== "" && (e.target.value < 1 || e.target.value > 31)) {
         e.target.parentNode.classList.add( "dayError", 'error');
+        return 0;
     }
     else{
         if (e.target.parentNode.classList.contains("emptyError")===false){
             e.target.parentNode.classList.remove("error", "dayError");
+            return 1;
         }
     }
 }
 const monthError = (e) => {
     if (e.target.value !== "" && (e.target.value < 1 || e.target.value > 12)) {
         e.target.parentNode.classList.add( "monthError", 'error');
+        return 0;
     }
     else{
         if (e.target.parentNode.classList.contains("emptyError")===false){
             e.target.parentNode.classList.remove("error", "monthError");
+            return 1;
         }
     }
 }
 const yearError = (e) => {
     if (e.target.value !== "" && e.target.value > currentYear) {
         e.target.parentNode.classList.add("error", "yearError");
+        return 0;
     }
     else{
         if (e.target.parentNode.classList.contains("emptyError")===false){
             e.target.parentNode.classList.remove("error", "yearError");
+            return 1;
         }
     }
 }
 const dateError = (e,m) => {
     if (e.target.value > daysInMonth[m.firstChild.value-1]){
         e.target.parentNode.classList.add("error", "dateError");
+        return 0;
     }
     else{
         if (e.target.parentNode.classList.contains("emptyError")===false){
             e.target.parentNode.classList.remove("error", "dateError");
+            return 1;
         }
         
     }
 }
 inputDays.addEventListener("input",function(e){
-    daysb4process=e.target.value;
-    emptyError(e);
-    dayError(e);
-    dateError(e,inputMonths);
+    okd= emptyError(e) * dayError(e) * dateError(e,inputMonths);
 
 })
 inputMonths.addEventListener("input",function(e){
-    monthsb4process=e.target.value;
-    emptyError(e);
-    monthError(e);
-    // if (monthsb4process == "") {
-    //     outputMonths.innerHTML="- -";
-    // }
+    okm= emptyError(e) * monthError(e);
 })
 inputYears.addEventListener("input",function(e){
-    emptyError(e);
-    yearError(e);
+    oky= emptyError(e) * yearError(e);
 })
 
 button.addEventListener("click",display);
